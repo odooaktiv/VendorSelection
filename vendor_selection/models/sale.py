4465f86e440554dbc2ec1272e3633389dbdcd5ad
Odoo Aktiv <solankiriddhir@yahoo.in>
2018-06-30 14:50:24 +0530

    [IMP] pep8 standards for branch 10.0

--- vendor_selection/models/sale.py
+++ vendor_selection/models/sale.py
@@ -1,34 +1,34 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api, _
-from odoo.exceptions import UserError, AccessError
+from odoo import models, fields, api
 
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-
     vendor_id = fields.Many2one('product.supplierinfo', 'Vendor')
     is_route = fields.Boolean('Is Route', defualt=False)
-    
+
     @api.multi
     @api.onchange('route_id')
     def _onchange_route_id(self):
         if self.route_id:
             self.is_route = True
-            
+
+
 class ProcurementOrder(models.Model):
     _inherit = 'procurement.order'
 
     def _make_po_select_supplier(self, suppliers):
-        """ Method intended to be overridden by customized modules to implement any logic in the
+        """ Method intended to be overridden by customized modules
+        to implement any logic in the
             selection of supplier.
         """
         if self:
             so = self.env['sale.order.line'].browse(self.sale_line_id.id)
             supplier = so.vendor_id or suppliers[0]
-            
+
             return supplier
         else:
-            return super(ProcurementRule, self)._make_po_select_supplier(values, suppliers)
-                     
+            return super(ProcurementOrder, self)\
+                ._make_po_select_supplier(suppliers)
