cae29dd1b015fa818cb3cd0706aa00da7a4b565e
Aktiv Software <odoo@aktivsoftware.com>
2021-05-11 11:45:15 +0530

    [ADD]: Added Module in v-14.

--- vendor_selection/__manifest__.py
+++ vendor_selection/__manifest__.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Vendor Selection',
+    'name': 'Vendor Selection For Dropshipping',
     'summary': 'Customer will select vendor as per his choice.',
     'website': 'http://www.aktivsoftware.com',
     'description': 'Customer will select vendor as per his choice in sale order \
         when customer choose drop shipping scenario.',
     'license': 'AGPL-3',
     'author': 'Aktiv Software',
-    'category': 'Sales',
-    'version': '12.0.1.0.0',
+    'category': 'Purchases',
+    'version': '14.0.1.0.0',
     'depends': ['sale_management', 'purchase', 'stock'],
     'data': [
         'views/sale_order.xml',
